1daacc867014b914bf33aba63c1f543d874ee895
Razieh Akbari <rakbari@hypertvx.com>
2023-04-03 21:26:01 +0200

    Handle Patient r4 struct

--- comfhirer/internal/application/fhir/r4/r4.go
+++ comfhirer/internal/application/fhir/r4/r4.go
@@ -426,90 +426,90 @@ type Extension struct {
 type Patient struct {
 
 	/** Resource Type Name (for serialization) */
-	ResourceType string `default"Patient"`
+	ResourceType string `json:"resourceType""`
 	/**
 	 * If a record is inactive, and linked to an active record, then future patient/record updates should occur on the other patient.
 	 */
-	Active  bool
-	_Active Element
+	Active  bool    `json:"active,omitempty"`
+	_Active Element `json:"_active,omitempty"`
 	/**
 	 * Patient may have multiple addresses with different uses or applicable periods.
 	 */
-	Address []Address
+	Address []Address `json:"address,omitempty"`
 	/**
 	 * At least an estimated year should be provided as a guess if the real DOB is unknown  There is a standard extension "patient-birthTime" available that should be used where Time is required (such as in maternity/infant care systems).
 	 */
-	BirthDate  string `json:"birthDate"`
-	_BirthDate Element
+	BirthDate  string  `json:"birthDate,omitempty"`
+	_BirthDate Element `json:"_birthDate,omitempty"`
 	/**
 	 * If no language is specified, this *implies* that the default local language is spoken.  If you need to convey proficiency for multiple modes, then you need multiple Patient.Communication associations.   For animals, language is not a relevant field, and should be absent from the instance. If the Patient does not speak the default local language, then the Interpreter Required Standard can be used to explicitly declare that an interpreter is required.
 	 */
-	Communication []PatientCommunication
+	Communication []PatientCommunication `json:"communication,omitempty"`
 	/**
 	 * Contact covers all kinds of contact parties family members, business contacts, guardians, caregivers. Not applicable to register pedigree and family ties beyond use of having contact.
 	 */
-	Contact []PatientContact
+	Contact []PatientContact `json:"contact,omitempty"`
 	/**
 	 * If there's no value in the instance, it means there is no statement on whether or not the individual is deceased. Most systems will interpret the absence of a value as a sign of the person being alive.
 	 */
-	DeceasedBoolean  bool
-	_DeceasedBoolean Element
+	DeceasedBoolean  bool    `json:"deceasedBoolean,omitempty"`
+	_DeceasedBoolean Element `json:"_deceasedBoolean,omitempty"`
 	/**
 	 * If there's no value in the instance, it means there is no statement on whether or not the individual is deceased. Most systems will interpret the absence of a value as a sign of the person being alive.
 	 */
-	DeceasedDateTime  string
-	_DeceasedDateTime Element
+	DeceasedDateTime  string  `json:"deceasedDateTime,omitempty"`
+	_DeceasedDateTime Element `json:"_deceasedDateTime,omitempty"`
 	/**
 	 * The gender might not match the biological sex as determined by genetics or the individual's preferred identification. Note that for both humans and particularly animals, there are other legitimate possibilities than male and female, though the vast majority of systems and contexts only support male and female.  Systems providing decision support or enforcing business rules should ideally do this on the basis of Observations dealing with the specific sex or gender aspect of interest (anatomical, chromosomal, social, etc.)  However, because these observations are infrequently recorded, defaulting to the administrative gender is common practice.  Where such defaulting occurs, rule enforcement should allow for the variation between administrative and biological, chromosomal and other gender aspects.  For example, an alert about a hysterectomy on a male should be handled as a warning or overridable error, not a "hard" error.  See the Patient Gender and Sex section for additional information about communicating patient gender and sex.
 	 */
-	Gender  Gender
-	_Gender Element
+	Gender  Gender  `json:"gender,omitempty"`
+	_Gender Element `json:"_gender,omitempty"`
 	/**
 	 * This may be the primary care provider (in a GP context), or it may be a patient nominated care manager in a community/disability setting, or even organization that will provide people to perform the care provider roles.  It is not to be used to record Care Teams, these should be in a CareTeam resource that may be linked to the CarePlan or EpisodeOfCare resources.
 	 * Multiple GPs may be recorded against the patient for various reasons, such as a student that has his home GP listed along with the GP at university during the school semesters, or a "fly-in/fly-out" worker that has the onsite GP also included with his home GP to remain aware of medical issues.
 	 * Jurisdictions may decide that they can profile this down to 1 if desired, or 1 per type.
 	 */
-	GeneralPractitioner []Reference
+	GeneralPractitioner []Reference `json:"generalPractitioner,omitempty"`
 	/**
 	 * An identifier for this patient.
 	 */
-	Identifier []Identifier
+	Identifier []Identifier `json:"identifier,omitempty"`
 	/**
 	 * There is no assumption that linked patient records have mutual links.
 	 */
-	Link []PatientLink
+	Link []PatientLink `json:"link,omitempty"`
 	/**
 	 * There is only one managing organization for a specific patient record. Other organizations will have their own Patient record, and may use the Link property to join the records together (or a Person resource which can include confidence ratings for the association).
 	 */
-	ManagingOrganization Reference
+	ManagingOrganization Reference `json:"managingOrganization,omitempty"`
 	/**
 	 * This field contains a patient's most recent marital (civil) status.
 	 */
-	MaritalStatus CodeableConcept
+	MaritalStatus CodeableConcept `json:"maritalStatus,omitempty"`
 	/**
 	 * Where the valueInteger is provided, the number is the birth number in the sequence. E.g. The middle birth in triplets would be valueInteger=2 and the third born would have valueInteger=3 If a boolean value was provided for this triplets example, then all 3 patient records would have valueBoolean=true (the ordering is not indicated).
 	 */
-	MultipleBirthBoolean  bool
-	_MultipleBirthBoolean Element
+	MultipleBirthBoolean  bool    `json:"multipleBirthBoolean,omitempty"`
+	_MultipleBirthBoolean Element `json:"_multipleBirthBoolean,omitempty"`
 	/**
 	 * Where the valueInteger is provided, the number is the birth number in the sequence. E.g. The middle birth in triplets would be valueInteger=2 and the third born would have valueInteger=3 If a boolean value was provided for this triplets example, then all 3 patient records would have valueBoolean=true (the ordering is not indicated).
 	 */
-	MultipleBirthInteger int
+	MultipleBirthInteger int `json:"multipleBirthInteger,omitempty"`
 	/**
 	 * A patient may have multiple names with different uses or applicable periods. For animals, the name is a "HumanName" in the sense that is assigned and used by humans and has the same patterns.
 	 */
-	Name []HumanName
+	Name []HumanName `json:"name,omitempty"`
 	/**
 	 * Guidelines
 	 * * Use id photos, not clinical photos.
 	 * * Limit dimensions to thumbnail.
 	 * * Keep byte count low to ease resource updates.
 	 */
-	Photo []Attachment
+	Photo []Attachment `json:"photo,omitempty"`
 	/**
 	 * A Patient may have multiple ways to be contacted with different uses or applicable periods.  May need to have options for contacting the person urgently and also to help with identification. The address might not go directly to the individual, but may reach another party that is able to proxy for the patient (i.e. home phone, or pet owner's phone).
 	 */
-	Telecom []ContactPoint
+	Telecom []ContactPoint `json:"telecom,omitempty"`
 }
 
 type AddressType string
