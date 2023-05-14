25fc0072b7a8287acd6a6778fc10224a91eef474
Razieh Akbari <rakbari@hypertvx.com>
2023-05-14 20:16:52 +0200

    survive panic

--- comfhirer/wiring.go
+++ comfhirer/wiring.go
@@ -2,9 +2,16 @@ package comfhirer
 
 import (
 	"github.com/rzeAkbari/comfhirer-server/comfhirer/internal/adapter"
+	"log"
 )
 
 func Run(input map[string]any) ([]byte, []error) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("panic occurred:", err)
+		}
+	}()
+
 	w := adapter.NewComfhirer()
 
 	return w.Comfhire(input)
